14b1c10d0843b9d9eac7756d482fff9d6b833d82
Lindsey Lazarevski <lindsey.gillum@gmail.com>
2019-10-08 12:34:37 -0400

    Include props and state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import SearchHeader from './searchheader';
 import BookList from './booklist';
+import Book from './book';
 
 class App extends React.Component{
   constructor(props){
@@ -15,19 +16,51 @@ class App extends React.Component{
   componentDidMount(){
     const url = 'https://www.googleapis.com/books/v1/volumes?';
     let params = {
-      q = []
-      key = AIzaSyDlsn3GHt3LSATXhaCSeSoXV0rPhTPf6jc;
+      q: [],
+      key: 'AIzaSyDlsn3GHt3LSATXhaCSeSoXV0rPhTPf6jc'
+    };
 
-    }
+    function getParams(params) {
+      const bookItems = Object.keys(params).map(
+        key =>
+        `${encodeURIComponent(key)}=${encodeURIComponent(bookItems[key])}`
+      );
+      return bookItems.join("&");
+      console.log(bookItems);
+    };
+    const booksearchurl = url + getParams(params);
+    console.log(booksearchurl);
     const options ={
       method: 'GET',
-    }
+    };
+
+    fetch(booksearchurl, options)
+      .then(res => {
+        if(!res.ok){
+          throw new Error('Something went wrong, please try again later')
+        }
+        return res;
+      })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          books: data,
+          error: null
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      });
   }
   render(){
       const page = this.state.showBookDetails
       ? <Book />
-      : <SearchHeader />
-        <BookList books= {this.state.books}>
+      : <div className ='bodycontainer'>
+          <SearchHeader />
+          <BookList books= {this.state.books}/>
+        </div>
   
   return (
     <div className="App">
