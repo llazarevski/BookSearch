d3b00b589c5e70e4ba15108c3326f507f90c59e6
Lindsey Lazarevski <lindsey.gillum@gmail.com>
2019-10-08 12:59:10 -0400

    Update Book component with return

--- src/book.js
+++ src/book.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './book.css';
 
 export default function Book(props){
+    return(
     <div className='book'>
         <header className='title'>{props.title}</header>/header>
         <p className='author'>{props.author}</p>
         <p className='price'>{props.price}</p>
         <p className='descripton'>{props.description}</p>    
     </div>
+    );
 }
